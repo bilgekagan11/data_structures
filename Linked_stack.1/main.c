82451e93334ff26904ac9c530f3a0b55a4a36a23
bilgekagan11 <147589367+bilgekagan11@users.noreply.github.com>
2023-10-11 10:21:17 +0300

    Create main.c

---
+++ Linked_stack.1/main.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct LINKED_STACK_NODE_s *LINKED_STACK_NODE;
+ typedef struct LINKED_STACK_NODE_s{
+     LINKED_STACK_NODE next;
+     void *data;
+     } LINKED_STACK_NODE_t[1];
+
+ typedef struct LINKED_STACK_s{
+     LINKED_STACK_NODE head;
+     } LINKED_STACK_t[1], *LINKED_STACK;
+
+ LINKED_STACK linked_stack_init();
+ void linked_stack_push(LINKED_STACK stack, void *data);
+ void *linked_stack_pop(LINKED_STACK stack);
+ void linked_stack_printint(LINKED_STACK stack);
+ void linked_stack_printchar(LINKED_STACK stack);
+void linked_stack_free(LINKED_STACK stack);
+void *linked_stack_top(LINKED_STACK stack);
+int linked_stack_is_emtpy(LINKED_STACK stack);
+LINKED_STACK linked_stack_init(){
+    LINKED_STACK  stack = (LINKED_STACK )malloc(sizeof (LINKED_STACK_t));
+    stack->head = NULL;
+    return stack;
+ }
+void linked_stack_push(LINKED_STACK stack, void *data){
+    LINKED_STACK_NODE  node  = (LINKED_STACK_NODE )malloc(sizeof (LINKED_STACK_NODE_t));
+    node->data = data;
+     if ( stack->head == NULL){
+         printf("stack is empty \n");
+         printf("first stack pushed\n");
+         node->next = NULL;
+         stack->head = node;
+     }else{
+         node->next = stack->head;
+         stack->head = node;
+         printf("stack is pushed \n");
+     }
+ }
+void *linked_stack_pop(LINKED_STACK stack){
+     if (stack->head == NULL){
+         printf("stack is empty\n");
+        return 0;
+     }else{
+         LINKED_STACK_NODE  temp = stack->head;
+         void * data = temp->data;
+         stack->head = temp->next;
+         free(temp);
+         return data;
+     }
+ }
+void linked_stack_printint(LINKED_STACK stack){
+    if (stack->head  == NULL){
+        printf("stack is empty\n");
+    }
+     else{
+        printf("\nprinting ... \n\n");
+        LINKED_STACK_NODE temp = stack->head;
+        int datax;
+        while (temp != NULL) {
+            datax = *(int *) temp->data;
+            printf("\n%d", datax);
+            temp = temp->next;
+        }
+     }
+ }
+void linked_stack_printchar(LINKED_STACK stack){
+    if (stack->head  == NULL){
+        printf("stack is empty\n");
+    }
+    else{
+        printf("\nprinting ... \n\n");
+        LINKED_STACK_NODE temp = stack->head;
+        char datax ;
+        while (temp != NULL) {
+            datax = *(char *) temp->data;
+            printf("\n%c", datax);
+            temp = temp->next;
+        }
+    }
+}
+void *linked_stack_top(LINKED_STACK stack){
+    if (stack->head == NULL){
+        printf("stack is empty\n");
+        return NULL;
+    }
+     else{
+        LINKED_STACK_NODE node = stack->head;
+        void *data = node->data;
+        return data;
+    }
+
+ }
+void linked_stack_free(LINKED_STACK stack){
+    while (stack->head != NULL){
+        linked_stack_pop(stack);
+    }
+    free(stack);
+}
+int linked_stack_is_emtpy(LINKED_STACK stack){
+     if (stack->head){
+         return 1;
+     }else return  0;
+ }
